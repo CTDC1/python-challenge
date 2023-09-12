8231ec019b5d9a093829a5c3777501362db5f438
CTDC1 <cameron_cullen1999@outlook.com>
2023-09-11 22:24:09 -0400

    Homework for Module 3

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -6,4 +6,87 @@ Created on Wed Sep  6 23:22:20 2023
 @author: cameroncullen
 """
 
-print ("Module 3")
+import os
+import csv
+
+# Path to collect data from the Resources folder
+# You need to specify the path to your CSV file here#
+
+
+# The total number of months included in the dataset
+cwd = os. getcwd ()
+
+#naming a variable, so we can later save the results in a text file 
+my_report = open("analysis/final_report.txt", "w")
+
+#calling from my computers documents to find the csv final we want to analysis
+csvpath = os.path.join(cwd, "Resources/budget_data.csv")
+
+#naming the variables
+found = False
+months = 0
+total = 0
+total_ch = 0
+pre_pl = 0
+inc = ["", 0]
+dec = ["", 0]
+
+#once we have called the csv file, we have to put it in a format that is readable, as well as tell the code how the columns are divided
+with open(csvpath, encoding='utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+#skip the first line of the csv file 
+    next(csvreader)
+    
+#we are counting the amount of months there are on the csv file and adding them to a variable 'months'
+    for row in csvreader:
+            months += 1
+            
+            
+ #we are summing up the total amount of value changes in the months by adding up the interger of the cells in column B
+            pl = int(row[1])
+            total += pl
+          
+#we are comparing the change between different months by comparing the cell we are in to the last cell
+# if the cell we are in is larger than the largest, then change the total_ch to the new change
+            change = pl - pre_pl
+            if pre_pl == 0:
+                change = 0
+            total_ch += change
+            
+#The greatest increase in profits (date and amount) over the entire period
+# if the change is larger than the last increase, then change the string to the month, and the value 
+            if change > inc[1]:
+                inc[0] = row[0]
+                inc[1] = change
+                
+                
+#The greatest decrease in profits (date and amount) over the entire period
+# if the change is largest than the largest decrease, then change the string to the month, and the value 
+
+            if change < dec[1]:
+                dec[0] = row[0]
+                dec[1] = change
+                
+            pre_pl = pl
+
+
+
+# we have created an f string or our dataprint. this is easier than writing multiple print lines
+# For Avgerage, Greatest Increase, Greatest Decrease, we are using string formatting solution to print the numerical number in the format that the question asked for
+# We also calcuate the Average Change in the print statement so we don't have to create an additional variable, and write extra lines of code
+
+output = f"""
+Financial Analysis
+----------------------------
+Total Months: {months}
+Total: ${total:,}
+Average Change: ${total_ch/(months-1):,.2f}
+Greatest Increase in Profits: {inc[0]} (${inc[1]:,})
+Greatest Decrease in Profits: {dec[0]} (${dec[1]:,})"""
+
+# print statement for output
+print(output)
+
+#creating a text document for our results 
+my_report.write(output)
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -6,4 +6,74 @@ Created on Wed Sep  6 23:22:20 2023
 @author: cameroncullen
 """
 
-print ("Module 3")
+# import necessary modules for homework
+import os
+import csv
+
+cwd = os. getcwd ()
+
+# creating variables that will be used later
+total_votes = 0 
+cans = {}
+winner = {}
+winnervotes =0
+
+#naming a variable, so we can later save the results in a text file 
+my_report = open("analysis/final_report.txt", "w")
+
+#calling from my computers documents to find the csv final we want to analysis
+csvpath = os.path.join(cwd, "Resources/election_data.csv")
+
+#once we have called the csv file, we have to put it in a format that is readable, as well as tell the code how the columns are divided
+with open(csvpath, encoding='utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+
+#skip the first row of the csv file   
+    next(csvreader)
+    
+# a for loop that loops though the csv, and reads the document. we know the votes are in column B and the candidates are in column 2
+#we add 1 to total votes so we know how many rows to count
+    for row in csvreader:
+          total_votes += 1
+          can = row[2]
+
+#this if loop determins in the name of the candidate name is in the dictonary 
+          if can not in cans.keys():
+#if they are not the votes count in set to zero                
+              cans[can] = 0
+#this moves increase the row number by 1, allowing us to move through the csv document              
+          cans[can] += 1
+                   
+         
+
+
+# we have name our ouput data as "output" so we do not have to create multiple print statements   
+# in line 61 we add on to the output statment. within that fstring, we are calculating the percentage of votes as well. 
+# in line 67 we are calculating the winner, with a for loop. we look at the candidate and their votes. if the candidates votes were 
+# larger than the previous, we change the name and the vote count.   
+output = f"""\nElection Results
+
+-------------------------
+Total Votes: {total_votes}
+
+-------------------------\n\n"""
+
+for can in cans.keys():
+    output += f"{can}: {cans[can]/total_votes * 100:.3f}% ({cans[can]})\n\n"
+
+
+output += f"-------------------------\n\n"
+
+for can, total_votes in cans.items():
+    if total_votes > winnervotes:
+        winner = can
+        winnervotes = total_votes
+        
+output+= f"Winner: {winner}\n\n"
+
+output += f"------------------------"
+
+#we are printing our statement, as well as saving it in a text file on my computer
+print(output)
+my_report.write(output)
